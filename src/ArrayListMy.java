ec80bb785d98e318eae4c60cbd69240a173f8934
sasha <alex.idimeshev@gmail.com>
2023-12-13 18:22:44 +0300

    Full project

--- src/ArrayListMy.java
+++ src/ArrayListMy.java
@@ -1,72 +1,105 @@
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
-public class ArrayListMy<E>{
+public class ArrayListMy<T> {
+    // 1. Параметризованный тип +
+    // 2. Конструктов, который позволяет передавать другую коллекцию +
+    // 3. Фабричный метод of* +
+    // 4. Comparator / сортировка +
+    // 5. Динамеческий массив +
     private int size;
 
-    private int[] element;
+    private T[] elements;
 
     private static final int DEFAULT_CAPACITY = 10;
 
     public ArrayListMy() {
-        this.element = new int[DEFAULT_CAPACITY];
+        this.elements = (T[]) new Object[DEFAULT_CAPACITY];
     }
+
+    public static ArrayListMy of(Object element) {
+        return new ArrayListMy(Collections.singleton(element));
+    }
+
     public ArrayListMy(int initCapacity) {
         if (initCapacity >= 0) {
-            this.element = new int[initCapacity];
+            this.elements =(T[]) new Object[initCapacity];
         } else {
             throw new IllegalStateException("Capacity can't be less than 0!");
         }
     }
 
-    public boolean addMy(int elem) {
-        element[size] = elem;
-        size++;
-        return true;
+    public ArrayListMy(Collection<? extends T> collection) {
+        if (!collection.isEmpty()) {
+            this.elements = (T[]) collection.toArray();
+            this.size = elements.length;
+        }
     }
-    public void addAllMy(ArrayListMy<E> elem) {
+
+    private boolean isEmpty() {
+        return false;
+    }
+
+    public void addMy(T elem) {
+        if (size == elements.length ) {
+            elements = Arrays.copyOf(elements, elements.length * 2);
+        }
+        elements[size] = elem;
+        size++;
+    }
+
+    public void addAllMy(ArrayListMy<? extends T> elem) {
         if (elem == null) {
             return;
         }
-        for (int i = 0; i < elem.size; i ++) {
-            addMy(elem.element[i]);
+        for (int i = 0; i < elem.size; i++) {
+            addMy((T) elem.elements[i]);
         }
     }
-    public Object getMy(int elem) {
-        return element[elem];
+
+    public T getMy(int elem) {
+        return (T) elements[elem];
     }
-    public int remove (int index) {
-        int[] temp = element;
-        element = new int[temp.length - 1];
-        int value = temp[index];
-        System.arraycopy(temp, 0, element, 0, index);
-        System.arraycopy(temp, index + 1, element, index, temp.length - index - 1);
+
+    public void removeMy(int index) {
+        Object[] temp = elements;
+        elements = (T[]) new Object[temp.length - 1];
+        System.arraycopy(temp, 0, elements, 0, index);
+        System.arraycopy(temp, index + 1, elements, index, temp.length - index - 1);
         size--;
-        return value;
     }
+
     public void clearMy() {
-        for (int i = 0; i < size; i ++) {
-            element[i] = 0;
+        for (int i = 0; i < size; i++) {
+            elements[i] = (T) new Object();
         }
         size = 0;
     }
-    public void bubbleSort() {
-        boolean isSorted = false;
-        int val;
-        while (!isSorted) {
-            isSorted = true;
-            for (int i = 0; i < element.length - 1; i ++) {
-                if (element[i] > element[i + 1]) {
-                    isSorted = false;
-                    val = element[i];
-                    element[i] = element[i + 1];
-                    element[i + 1] = val;
+    public void sort(Comparator<? super T> comparator) {
+        boolean flag = true;
+        while (flag) {
+            flag = false;
+            for (int i = 0; i < elements.length - 1; i++) {
+                if (comparator.compare(elements[i], elements[i + 1]) > 0) {
+                    swap(i, i + 1);
+                    flag = true;
                 }
             }
         }
     }
-
+    public static <E> void sort(ArrayListMy<? extends E> list, Comparator<? super E> comparator) {
+        list.sort(comparator);
+    }
+    private void swap(int first, int second) {
+        T temp = (T) elements[first];
+        elements[first] = elements[second];
+        elements[second] = temp;
+    }
     @Override
     public String toString() {
-        return Arrays.toString(element);
+        return Arrays.toString(elements);
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
         ArrayListMy<Integer> arrayListMy = new ArrayListMy<>();
+        ArrayListMy<Integer> arrayListMy1 = new ArrayListMy<>();
+        ArrayListMy<Integer> arrayListMy2 = ArrayListMy.of("Sasha");
 
         arrayListMy.addMy(1);
         arrayListMy.addMy(10);
-        arrayListMy.addMy(22);
+        arrayListMy.addMy(20);
+        arrayListMy.addMy(100);
         arrayListMy.addMy(5);
-        arrayListMy.addMy(3);
-        arrayListMy.addMy(12);
-        arrayListMy.addMy(40);
-        arrayListMy.addMy(22);
-        arrayListMy.addMy(8);
+        arrayListMy.addMy(35);
         arrayListMy.addMy(2);
+        arrayListMy.addMy(123);
+        arrayListMy.addMy(8);
+        arrayListMy.addMy(7);
+        System.out.println(arrayListMy);
 
         System.out.println(arrayListMy);
 
-        arrayListMy.bubbleSort();
+        arrayListMy1.addAllMy(arrayListMy);
         System.out.println(arrayListMy);
 
-        arrayListMy.clearMy();
+        System.out.println(arrayListMy.getMy(2));
+
+        arrayListMy.removeMy(2);
         System.out.println(arrayListMy);
+
+        System.out.println(arrayListMy2);
 
     }
 }
